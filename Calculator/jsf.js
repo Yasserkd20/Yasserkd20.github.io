a95e6d4de62a8152e779828d1e8e27c555c2f87a
Yasserk142 <141807748+Yasserkd20@users.noreply.github.com>
2023-08-09 08:04:03 +0100

    up

---
+++ Calculator/jsf.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+    //
+    const display=document.getElementById("display");
+    //
+    document.getElementById("ac").addEventListener("click",function() { display.value="";});
+    document.getElementById("de").addEventListener("click",
+    function() { 
+        var a=display.value.toString();
+        a=a.slice(0,-1);
+        display.value=a;
+    });
+    //
+    var tab=document.getElementsByName("btn");
+    for(var i=0;i<16;i++){
+        tab[i].addEventListener("click",function() { display.value+=this.value;});
+        //tab[i].onclick=function(){ display.value+=this.value;};
+    }
+    //
+    const equal=document.getElementById("equal");
+    equal.addEventListener("click",function() { display.value=eval(display.value);});
+    
+    //
+}
